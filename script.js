26c9fdb8a9c8ddb4ec73937e62d75b3107b33118
Matt Dennis <dennis57@msu.edu>
2017-01-31 16:48:18 -0500

    added script.js adding functionality to buttons

---
+++ script.js
@@ -0,0 +1,35 @@
+var hiHatButton = document.getElementById('hi-hat-button')
+
+var hiHatSound = document.getElementById('hi-hat-sound')
+
+hiHatButton.addEventListener('click', function () {
+  hiHatSound.play()
+})
+
+hiHatButton.addEventListener('mouseenter', function () {
+  hiHatSound.play()
+})
+
+var snareButton = document.getElementById('snare-button')
+
+var snareSound = document.getElementById('snare-sound')
+
+snareButton.addEventListener('click', function () {
+  snareSound.play()
+})
+
+snareButton.addEventListener('mouseenter', function () {
+  snareSound.play()
+})
+
+var bassButton = document.getElementById('bass-button')
+
+var bassSound = document.getElementById('bass-sound')
+
+bassButton.addEventListener('click', function () {
+  bassSound.play()
+})
+
+bassButton.addEventListener('mouseenter', function () {
+  bassSound.play()
+})
